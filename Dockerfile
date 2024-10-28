47c4bf5be03f3d0c3ffd1323f1c9f984f145d136
blackjune67 <buea486@gmail.com>
2024-10-28 17:04:06 +0900

    도커 이미지 스크립트 작성

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# 베이스 이미지 설정
+FROM python:3.9-slim
+
+# 작업 디렉터리 생성
+WORKDIR /app
+
+# 요구사항 파일을 컨테이너로 복사
+COPY backend/requirements.txt .
+
+# 패키지 설치
+RUN pip install --no-cache-dir -r requirements.txt
+
+# YOLO 모델 파일과 애플리케이션 코드를 복사
+COPY yolo11n.pt . 
+COPY backend/app /app
+
+# 컨테이너 외부에서 접근할 수 있도록 포트 설정
+EXPOSE 8000
+
+# 애플리케이션 실행
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,16 @@
+import logging
 from fastapi import FastAPI, WebSocket
 from app.api.websocket import router
+# from prometheus_fastapi_instrumentator import Instrumentator
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
 app = FastAPI()
 
-# API 컨트롤러
+# instrumentator = Instrumentator().instrument(app)
+# instrumentator.expose(app, include_in_schema=False)
 
+# API 컨트롤러
 app.include_router(router)
 
 if __name__ == "__main__":
