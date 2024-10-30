ef8a4bdfa708d43bad2a9cc3746e9e6f7820ddf3
blackjune67 <buea486@gmail.com>
2024-10-30 14:35:13 +0900

    main source code modify

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,15 @@ FROM python:3.9-slim
 # 작업 디렉터리 생성
 WORKDIR /app
 
+# YOLO 모델 파일과 애플리케이션 코드를 복사 (자주변경 되는 부분을 상단에 배치)
+COPY yolo11n.pt . 
+COPY backend/app /app
+
 # 요구사항 파일을 컨테이너로 복사
 COPY backend/requirements.txt .
 
 # 패키지 설치
 RUN pip install --no-cache-dir -r requirements.txt
-
-# YOLO 모델 파일과 애플리케이션 코드를 복사
-COPY yolo11n.pt . 
-COPY backend/app /app/app
 
 # 컨테이너 외부에서 접근할 수 있도록 포트 설정
 EXPOSE 8000
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,27 +1,8 @@
-import logging
+# import logging
 from fastapi import FastAPI, WebSocket
-from app.api.websocket import router
-# from prometheus_fastapi_instrumentator import Instrumentator
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-# 콘솔에 로그 출력
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-logger.addHandler(console_handler)
-
-# 파일에 로그 저장 (localLog.log)
-file_handler = logging.FileHandler("/Users/highbuff/Repository/gender_analysis_vision/logs/localLog.log")
-file_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-logger.addHandler(file_handler)
-
-# logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+from api.websocket import router
 
 app = FastAPI()
-
-# instrumentator = Instrumentator().instrument(app)
-# instrumentator.expose(app, include_in_schema=False)
 
 # API 컨트롤러
 app.include_router(router)
